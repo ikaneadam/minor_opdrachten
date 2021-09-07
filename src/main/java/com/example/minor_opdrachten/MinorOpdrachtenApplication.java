e1bf3a26bac1ce339cd308efca5dc5c971adad85
s1122409 <Ikaneadam@gmail.com>
2021-09-07 11:56:12 +0200

    Initial commit

---
+++ src/main/java/com/example/minor_opdrachten/MinorOpdrachtenApplication.java
@@ -0,0 +1,13 @@
+package com.example.minor_opdrachten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinorOpdrachtenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinorOpdrachtenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/minor_opdrachten/MinorOpdrachtenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minor_opdrachten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinorOpdrachtenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
